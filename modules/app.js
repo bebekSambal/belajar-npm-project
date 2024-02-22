aa984cfde578147c9ce5c916dbd628a37bf208c0
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-02-22 21:29:58 +0700

    Update app.js
    punya beberapa modul jenis2 kucing yang disatukan kedalam file index lalu index tsb dipanggil ke app js

--- modules/app.js
+++ modules/app.js
@@ -2,7 +2,9 @@
 const rumus = require('./rumus'); // cara 1
 // const {jumlah, keliling, luasLingkaran} = require('./rumus'); // cara 2
 
+const cats = require('../module/cats/index')
 
 console.log(rumus.jumlah(100,210));
 console.log(rumus.keliling(7));
 console.log(rumus.luasLingkaran(10));
+console.log(cats[0]);
